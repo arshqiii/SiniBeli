1cf00d999a2066f34ef5aa6bfc0a66d706333462
arshqii <marshq23@gmail.com>
2024-09-07 13:43:33 +0700

    migrated changes to model & fixed TemplateNotFound Exception

--- e_commerce_pbp/settings.py
+++ e_commerce_pbp/settings.py
@@ -52,10 +52,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'e_commerce_pbp.urls'
 
+import os
+SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(SETTINGS_PATH, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
