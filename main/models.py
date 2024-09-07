63b54baa8afdd2c40dcbeb9b4166db92d7b3277b
arshqii <marshq23@gmail.com>
2024-09-07 13:27:30 +0700

    added model & view + configured url

--- main/models.py
+++ main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    description = models.TextField()
+
+    
+    @property
+    def get_name(self):
+        return self.name
+    
+    @property
+    def get_price(self):
+        return f"Rp{self.price:,.0f}"
+    
+    @property
+    def get_desc(self):
+        return self.description
--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_main(request):
+    context = {
+        'name' : 'Hoodie',
+        'price': 150000,
+        'description': 'Hoodie adalah hoodie'
+    }
+
+    return render(request, "main.html", context)
