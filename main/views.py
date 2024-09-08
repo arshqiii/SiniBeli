73f3c2af46ba4ecf57304786d0ed83dff644e100
arshqii <marshq23@gmail.com>
2024-09-08 20:54:39 +0700

    added few attributes to view.py & moved template to main folder

--- main/views.py
+++ main/views.py
@@ -3,9 +3,23 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'name' : 'Hoodie',
-        'price': 150000,
-        'description': 'Hoodie adalah hoodie'
+        'nama' : 'Muhammad Radhiya Arshq',
+        'npm' : '2306275885',
+        'kelas' : 'PBP D',
+
+        'app_name' : 'SiniBeli',
+        'app_desc' : 'Welcome to SiniBeli',
+
+        'product_name1' : 'Iphone 15',
+        'price1': 15000000,
+        'description1': '',
+        'product_name2' : 'Laptop Asus ROG Zephyrus',
+        'price2': 18500000,
+        'description3': '',
+        'product_name3' : 'Sony Noise-Cancelling Headphones',
+        'price3': 3000000,
+        'description3': '',
+
     }
 
     return render(request, "main.html", context)
