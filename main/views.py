7e019bf749a07594c6994452d35fee63d390bd0c
arshqii <marshq23@gmail.com>
2024-09-15 22:38:12 +0700

    updated urls.py + views.py for xml & json data viewing

--- main/views.py
+++ main/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.core import serializers
 from main.forms import ProductEntryForm
 from main.models import Product
 
 # Create your views here.
+def show_xml(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+
+def show_xml_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+
 def show_main(request):
     product_entries = Product.objects.all()
 
