755842c5f063c27aa8180a7baa05c1beab7a5ed7
arshqii <marshq23@gmail.com>
2024-09-21 21:07:28 +0700

    added register function and register html

--- main/views.py
+++ main/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.core import serializers
 from main.forms import ProductEntryForm
 from main.models import Product
+from django.contrib.auth.forms import UserCreationForm #ini memudahkan pembuatan formulir pendaftaran pengguna dalam aplikasi web.
+from django.contrib import messages
 
 # Create your views here.
 def show_main(request):
@@ -44,6 +46,19 @@ def create_product(request):
 
     context = {'form': form}
     return render(request, "create_product.html", context)
+
+def register(request): #berfungsi untuk menghasilkan formulir registrasi secara otomatis dan menghasilkan akun pengguna ketika data di-submit dari form.
+    form = UserCreationForm() #menggunakan hasil import sebagai basis dari form
+
+    if request.method == "POST": #cek jika metode request == POST
+        # membuat UserCreationForm baru dari yang sudah di-impor sebelumnya dengan memasukkan QueryDict berdasarkan input dari user pada request.POST
+        form = UserCreationForm(request.POST) 
+        if form.is_valid(): 
+            form.save() # membuat dan menyimpan data dari form tersebut jika valid
+            messages.success(request, 'Your account has been successfully created!') #menampilkan pesan kepada pengguna setelah melakukan suatu aksi.
+            return redirect('main:login') #redirect setelah data form berhasil disimpan/masuk ke halaman login setelah register akun
+    context = {'form':form}
+    return render(request, 'register.html', context) #form akan dirender dalam file register.html yang berisi context
 
 def show_xml(request):
     data = Product.objects.all()
